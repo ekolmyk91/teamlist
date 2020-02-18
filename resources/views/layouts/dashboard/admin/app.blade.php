9609d9ce18346435ae432abbb388cc04559737f6
dmitriymykolenko <dmitriy.mykolenko@ukad-group.com>
2020-02-18 17:56:05 +0200

    Added datepicker js,css

--- resources/views/layouts/dashboard/admin/app.blade.php
+++ resources/views/layouts/dashboard/admin/app.blade.php
@@ -15,6 +15,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
     <!-- Material Kit CSS -->
     <link href="{{ asset('css/dashboard/material-dashboard.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.css" />
+
 </head>
 
 <body>
@@ -190,6 +192,9 @@
 <script src="{{ asset('js/dashboard/core/popper.min.js') }}"></script>
 <script src="{{ asset('js/dashboard/core/bootstrap-material-design.min.js') }}"></script>
 <script src="{{ asset('js/dashboard/plugins/perfect-scrollbar.jquery.min.js') }}"></script>
+<script src="{{ asset('js/dashboard/moment.min.js') }}"></script>
+<script src="{{ asset('js/dashboard/bootstrap-datetimepicker.min.js') }}"></script>
+<script src="{{ asset('js/dashboard/material-dashboard.js') }}"></script>
 </body>
 
 </html>
