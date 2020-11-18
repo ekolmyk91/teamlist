915db408bcb8a8cfdd092bc98fe71d0d93c938db
Zuborev <zuborev@myhost>
2020-11-19 00:04:31 +0200

    api controller update

--- app/Http/Controllers/API/MemberController.php
+++ app/Http/Controllers/API/MemberController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Member;
 use App\Department;
 use App\Position;
+use App\Certificate;
 use App\User;
 use Carbon\Carbon;
 use Image;
@@ -23,7 +24,7 @@ class MemberController extends Controller
             'email',
             'department_id',
             'position_id',
-        ])->with('user:id,avatar,active', 'department:id,name', 'position:id,name');
+        ])->with('user:id,avatar,active', 'department:id,name', 'position:id,name', 'certificates:id,logo');
 
         if ($request->filled('department')) {
             $members->where('department_id', $request->get('department'));
@@ -38,18 +39,19 @@ class MemberController extends Controller
 
     public function show($id)
     {
-        $member = Member::with('user:id,avatar', 'department:id,name', 'position:id,name')->find($id, [
-          'user_id',
-          'name',
-          'surname',
-          'birthday',
-          'email',
-          'phone_1',
-          'phone_2',
-          'about',
-          'department_id',
-          'position_id',
-        ]);
+        $member = Member::with('user:id,avatar', 'department:id,name', 'position:id,name', 'certificates:id,logo')
+                        ->find($id, [
+                                  'user_id',
+                                  'name',
+                                  'surname',
+                                  'birthday',
+                                  'email',
+                                  'phone_1',
+                                  'phone_2',
+                                  'about',
+                                  'department_id',
+                                  'position_id',
+                                ]);
 
         return response()->json($member);
     }
