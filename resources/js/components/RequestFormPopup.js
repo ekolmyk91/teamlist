5fc73726601617301c6fdbd82bf4367f8202cb81
olreshetniak <oreshetniak@corp.web4pro.net>
2022-10-19 16:25:06 +0300

    update popup and validation

--- resources/js/components/RequestFormPopup.js
+++ resources/js/components/RequestFormPopup.js
@@ -8,6 +8,41 @@ import NativeSelect from '@material-ui/core/NativeSelect';
 import InputLabel from '@material-ui/core/InputLabel';
 import {postRequest, getOffTimeTipes} from '../api/Api'
 
+import { styled } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { min } from 'lodash';
+
+
+const RequestButton = styled(Button)({
+    background: 'transparent',
+    color: '#0e698a',
+    padding: '10px',
+    border: '1px solid #0e698a',
+    borderRadius: '0',
+    marginBottom: '2rem',
+    '&:hover': {
+        background: '#0e698a',
+        borderColor: '#0e698a',
+        color: 'white'
+    }
+});
+
+const ErrorShow = {
+    display: 'block',
+    color: 'red',
+    textAlign: 'center'
+}
+
+const ErrorHide = {
+    display: 'done'
+}
+
+const SuccessShow = {
+    display: 'block',
+    color: 'green',
+    textAlign: 'center'
+}
+
 
 
 class RequestFormPopup extends Component {
@@ -19,14 +54,11 @@ class RequestFormPopup extends Component {
 			selectedDateFinish: '',
             positions: [],
             selectValue: '',
+            status: ErrorHide,
+            messages: [],
+            minDate: new Date()
 		}
 	}
-    updateDate(date) {
-        let curr_date = date.getDate();
-        let curr_month = date.getMonth() + 1;
-        let curr_year = date.getFullYear();
-        return curr_year + "-" + curr_month + "-" + curr_date
-    }
 
     componentDidMount () {
         getOffTimeTipes().then(response => {
@@ -39,23 +71,81 @@ class RequestFormPopup extends Component {
 		})
     }
 
+    numberСheck(num) {
+        if(String(num).length === 1) {
+            let updateNum = '0' + String(num)
+            return updateNum
+        } else {
+            return num
+        }
+    }
+
+    updateDate(date) {
+        let curr_date = date.getDate();
+        let curr_month = date.getMonth() + 1;
+        let curr_year = date.getFullYear();
+
+        return curr_year + "-" + this.numberСheck(curr_month) + "-" + this.numberСheck(curr_date)
+    }
+
     handleDateChangeStart = (date) => {
         this.setState({selectedDateStart: this.updateDate(date)});
     }
+
     handleDateChangeFinish = (date) => {
         this.setState({selectedDateFinish: this.updateDate(date)});
     }
+
     handleChangeSelect = (e) => {
         this.setState({selectValue: e.target.value});
     }
+
     handleSubmit = (e) => {
         e.preventDefault();
-        let data = $('.request__form').serialize()
-        console.log(data)
-        postRequest(data).then(response => {
-			console.log(response)
-		})
+        this.setState({status: ErrorHide});
+        this.setState({messages: []});
+
+        if(new Date(this.state.selectedDateStart) <= new Date(this.state.selectedDateFinish)) {
+            let date = new Date();
+            date.setDate(date.getDate() + 2)
+
+            if(+this.state.selectValue === 1 && new Date(date) > new Date(this.state.selectedDateStart)) {
+                this.setState({status: ErrorShow});
+                this.setState({messages: ['when choosing a vacation, start at least 3 days in advance']});
+            } else {
+                let data = $('.request__form').serialize();
+                postRequest(data)
+                    .then(response => {
+                        this.setState({status: SuccessShow});
+                        this.setState({messages: [`${response.message}`]});
+                    })
+                    .catch(error => {
+                        let errors = error.response.data.message
+                        if (Object.keys(errors).length > 0) {
+                            this.setState({status: ErrorShow});
+                            let arr = []
+                            Object.keys(errors).map(key => {
+                                arr.push(errors[key][0]);
+                            })
+                            this.setState({messages: arr});
+                        }
+                    })
+            }
+        } else {
+            this.setState({status: ErrorShow});
+            this.setState({messages: ['end date must not be less than start date']});
+        }
     }
+
+    resetForm = (e) => {
+        this.props.closePopup();
+        this.setState({selectValue: this.state.positions[0].id});
+        this.setState({selectedDateStart: this.updateDate(new Date())});
+        this.setState({selectedDateFinish: this.updateDate(new Date())});
+        this.setState({status: ErrorHide});
+        this.setState({messages: []});
+    }
+
     render () {
         const { t } = this.props;
 
@@ -63,15 +153,23 @@ class RequestFormPopup extends Component {
             return (
                 <div className='request__popup'>
                     <div className="close-icon">
-                        <span href='#' onClick={this.props.closePopup}>{t(data.close)}</span>
+                        <span href='#' onClick={this.resetForm}>{t(data.close)}</span>
                     </div>
                     <form onSubmit={this.handleSubmit} className='request__form'>
                         <div className='request__form__dates'>
                             <input type="hidden" name="user_id" id="user_id" value={this.props.user.roles[0].pivot.user_id}></input>
                             <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                                <DatePicker value={ this.state.selectedDateStart} onChange={(date) => this.handleDateChangeStart(date)} label="start day"/>
+                                <DatePicker
+                                    value={ this.state.selectedDateStart}
+                                    onChange={(date) => this.handleDateChangeStart(date)}
+                                    label="start day"
+                                    minDate={this.state.minDate} />
                                 <input type="hidden" name="start_day" id="start_day" value={ this.state.selectedDateStart}></input>
-                                <DatePicker value={ this.state.selectedDateFinish} onChange={(date) => this.handleDateChangeFinish(date)} label="end day"/>
+                                <DatePicker
+                                    value={ this.state.selectedDateFinish}
+                                    onChange={(date) => this.handleDateChangeFinish(date)}
+                                    label="end day"
+                                    minDate={this.state.minDate}/>
                                 <input type="hidden" name="end_day" id="end_day" value={ this.state.selectedDateFinish}></input>
                             </MuiPickersUtilsProvider>
                         </div>
@@ -92,8 +190,9 @@ class RequestFormPopup extends Component {
                                 })}
                             </NativeSelect>
                         </FormControl>
-                        <input type="submit" value="Submit" className='request__form__btn'/>
+                        <RequestButton type="submit" className='request__form__btn'>Submit</RequestButton>
                     </form>
+                    {this.state.messages.map(err => (<p style={this.state.status} key={err}>{err}</p>))}
                 </div>
             )
         } else {
