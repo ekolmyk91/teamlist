381ebd3dabc3ad7b2be3195fbaad74251af2ae15
Azuborev <azuboriev>
2026-06-26 14:54:09 +0300

    fix curl

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,9 @@
 <?php
 
+if (!defined('CURL_SSLVERSION_TLSv1_2')) {
+    define('CURL_SSLVERSION_TLSv1_2', 6);
+}
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
